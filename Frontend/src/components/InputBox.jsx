ebb05315b43a472671f18c9b22c8a05c66143e87
Pradeep yadav <pradeep20020102@gmail.com>
2024-12-23 23:27:11 +0545

    half frontend is done

--- Frontend/src/components/InputBox.jsx
+++ Frontend/src/components/InputBox.jsx
@@ -1,8 +1,8 @@
-export function InputBox({ label, placeholder }) {
+export function InputBox({ label, placeholder, onChange }) {
   return (
     <div>
-      <div className="text-sm font-medium text-left py-2">{label}</div>
-      <input type="text" placeholder={placeholder} className="w-full px-2 py-1"/>
+      <div className="text-sm font-medium text-left py-2 border-black">{label}</div>
+      <input onChange={onChange} type="text" placeholder={placeholder} className="w-full px-2 py-1 rounded-md  border-solid border-2 border-black-400  bg-white  hover:bg-gray-100 focus:outline-none focus:ring focus:ring-blue-400 ..."/>
     </div>
   );
 }
